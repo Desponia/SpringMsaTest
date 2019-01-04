bd7d187d985ea9035710dc54903f65eac1b97c65
desponia <coalcellar@sk.com>
2019-01-04 14:37:24 +0900

    hystrix fallback method 실행

--- display/src/main/java/com/elevenst/DisplayApplication.java
+++ display/src/main/java/com/elevenst/DisplayApplication.java
@@ -2,9 +2,11 @@ package com.elevenst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @SpringBootApplication
 public class DisplayApplication {
 
@@ -16,5 +18,6 @@ public class DisplayApplication {
     public static void main(String[] args) {
         SpringApplication.run(DisplayApplication.class);
     }
+
 
 }
