8c6618240dc0a094e8d3605027c354c60c9c215d
desponia <coalcellar@sk.com>
2019-01-04 16:58:45 +0900

    feign - ribbon 연동

--- display/src/main/java/com/elevenst/service/FeignProductRemoteService.java
+++ display/src/main/java/com/elevenst/service/FeignProductRemoteService.java
@@ -4,7 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "product", url = "http://localhost:8082/")
+@FeignClient(name = "product")
+//@FeignClient(name = "product", url = "http://localhost:8082/")
 public interface FeignProductRemoteService {
     @RequestMapping(path = "/products/{productId}")
     String getProductInfo(@PathVariable("productId") String productId);
